51ffaae4b40bb2a95ffe57bd12bb47ef3b6857bb
Santhosh kuamr <santhoshfsdev@gmail.com>
2020-04-15 09:30:54 -0400

    command chnage

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.use(express.static('/client/build'));
 }
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     if (process.env.NODE_ENV === 'production') {
 
         app.use(express.static('client/build'));
