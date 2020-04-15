420cf779b829799fdac657465e0325275d5a0e72
Santhosh kuamr <santhoshfsdev@gmail.com>
2020-04-15 09:05:37 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+
+
+import io from 'socket.io-client';
+import './App.css';
+
+const socket = io("https://santxt.herokuapp.com/");
+socket.on('connect', () => {
+  console.log("connected")
+});
+
+function App() {
+
+  const [text, setText] = useState('');
+  const [msg, setMsg] = useState([]);
+
+  useEffect(() => {
+    socket.once('text', (data) => {
+      console.log(data)
+      setMsg(msg.concat(data));
+      setText('')
+    });
+  });
+
+  return (
+    <div className="App">
+      <h1> Messages</h1>
+      <hr />
+      <div>
+        {msg.map((msg, index) => {
+
+
+          return (
+            <div>
+              <p key={index} color="dark">
+                <h5>{msg}</h5>
+              </p>
+            </div>
+          )
+
+        })
+        }
+      </div>
+      <input value={text} placeholder="Enter Input" onChange={e => setText(e.target.value)}/>
+        <button onClick={()=>{
+          socket.emit('text',text);
+        }}
+        color="dark">Send</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+
+const io = require('socket.io')(PORT);
+
+io.on("connection", (socket) => {
+    console.log('Connection connected');
+    socket.on('text', (data)=>{
+        console.log(data)
+
+        io.emit('text',data);
+    });
+    
+socket.on("disconnect", () => {console.log("Client disconnected") })
+})
+
+
+if (process.env.NODE_ENV === 'production') {
+
+    app.use(express.static('/myApp/build'));
+}
+app.get("/", (req, res) => {
+    res.send({ response: "I am alive" }).status(200);
+  });
+
+
+
