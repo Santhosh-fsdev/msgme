9d937153baf64c3697d4e2c7ff330b8b083973ee
Santhosh kuamr <santhoshfsdev@gmail.com>
2020-04-15 11:10:35 -0400

    http server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const server = require('http').Server(app)
+
+
 
 const PORT = process.env.PORT || 8080;
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-
-const io = require('socket.io')(PORT);
+const io = require('socket.io')(server);
 
 io.on("connection", (socket) => {
     console.log('Connection connected on port'+ PORT );
@@ -22,9 +26,13 @@ socket.on("disconnect", () => {console.log("Client disconnected") })
 
 if (process.env.NODE_ENV === 'production') {
 
-    app.use(express.static('client/build'));
+    app.use(express.static('client/build/index.html'));
+
 }
 
+server.listen(PORT,()=>{
+    console.log(`server is listening on ${PORT}`)
+})
 
 
 
